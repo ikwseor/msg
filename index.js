7e5ec2e9c044c2af6f74b3e133331148b0900871
ikwseor <brustvic3@gmail.com>
2025-06-10 22:46:14 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+
+TOKEN = 'MTM4MjE3MzE5Mjc4OTE2NDE3Mw.GGGNiU.Nz4s1mUPlnIh_NVZ-NtbN9Oho4L35Fo2oQQX2U'
+TARGET_USER_ID = 890768957374615573 # ID do usuário que você quer limpar 
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logado como {bot.user.name}')
+
+    for guild in bot.guilds:
+        print(f'Apagando mensagens em: {guild.name}')
+        for channel in guild.text_channels:
+            try:
+                print(f'→ Canal: {channel.name}')
+                async for message in channel.history(limit=None):
+                    if message.author.id == TARGET_USER_ID:
+                        try:
+                            await message.delete()
+                        except:
+                            pass
+            except Exception as e:
+                print(f'Erro no canal {channel.name}: {e}')
+
+    print('✅ Limpeza finalizada.')
+    await bot.close()
+
+bot.run(TOKEN)
